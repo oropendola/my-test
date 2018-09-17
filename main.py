b822b0f007d974f1188c538104add7d32cf174c7
Javier Felipe Toribio <highavoc@yahoo.es>
2018-09-17 03:39:17 +0100

    commit all

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+# @ Javier Felipe Toribio 2018
+#
+# abstract: Testing git
+#
+from test import Test
+
+
+test1 = Test(1,"Test one")
+print(test1)
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-
-
+#
+# @ Javier Felipe Toribio 2018
+#
+# abstract: class for testing git
+#
 class Test:
   def __init__(self,id,name):
     self.id=id
     self.name=name
   def __str__(self):
     return "Id: {} Name: {}".format(self.id,self.name)
+
+if __name__ == "__main__":
     
-t = Test(1,"test")
-print(t)
+    t = Test(1,"test")
+    print(t)
